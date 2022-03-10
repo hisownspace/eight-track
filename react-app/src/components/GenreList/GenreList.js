1d64b6fb3f5198ab922cf7d8f28ca83d7a8fdd89
David Nash <hisownspace@gmail.com>
2022-03-10 13:54:33 -0500

    fix carousel for mobile

--- react-app/src/components/GenreList/GenreList.js
+++ react-app/src/components/GenreList/GenreList.js
@@ -6,16 +6,21 @@ import { Link } from 'react-router-dom';
 
 const responsive = {
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 5,
-      slidesToSlide: 2
+        breakpoint: { max: 3000, min: 1024 },
+        items: 5,
+        slidesToSlide: 2
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 3,
+        slidesToSlide: 3,
     },
     mobile: {
-        breakpoint: { max: 464, min: 400 },
-        items: 1,
-        slidesToSlide: 1,
+        breakpoint: { max: 464, min: 0 },
+        items: 3,
+        slidesToSlide: 3,
     }
-  };
+};
 
 function GenreList({ genreId }) {
     const id = +genreId;
