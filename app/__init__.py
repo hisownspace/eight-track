f535e864859bae2e25af7f28f062f7721fd1184b
David Nash <hisownspace@gmail.com>
2022-12-07 11:47:17 -0500

    alter package.json and favicon pathing in __init__

--- app/__init__.py
+++ app/__init__.py
@@ -86,5 +86,5 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+        return app.send_from_directory('public', 'favicon.ico')
     return app.send_static_file('index.html')
