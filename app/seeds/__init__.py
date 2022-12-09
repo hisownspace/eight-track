4c3b960167328f59e3c90360db157d23875202e8
David Nash <hisownspace@gmail.com>
2022-12-09 13:45:11 -0500

    add seed song file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .genres import seed_genres, undo_genres
+from .songs import seed_songs, undo_songs
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_genres()
+    seed_songs()
     # Add other seed functions here
 
 
@@ -20,4 +22,5 @@ def seed():
 def undo():
     undo_users()
     undo_genres()
+    undo_songs()
     # Add other undo functions here
