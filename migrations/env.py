6d5a8e7ddc3e3ad9f1c1d9edee0b75430309dcad
David Nash <hisownspace@gmail.com>
2022-12-12 15:47:14 -0500

    alter alembic env.py to handle SCHEMA

--- migrations/env.py
+++ migrations/env.py
@@ -1,3 +1,4 @@
+
 from __future__ import with_statement
 
 import logging
@@ -7,6 +8,11 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+import os
+production = os.getenv("FLASK_DEBUG") == "False"
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -35,15 +41,12 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -56,10 +59,8 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     # this callback is used to prevent an auto-migration from being generated
@@ -85,10 +86,15 @@ def run_migrations_online():
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
+        # Create a schema (only in production)
+        if production:
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA};")
 
+        # Set search path to your schema (only in production)
         with context.begin_transaction():
+            if production:
+                context.execute(f"SET search_path TO {SCHEMA};")
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
