f212df7279d716b18a6b0b35f3604a794ee136d8
David Nash <hisownspace@gmail.com>
2023-08-08 23:09:32 -0400

    rearrange player artist and title, add image

--- react-app/src/components/FooterPlayer/FooterPlayer.js
+++ react-app/src/components/FooterPlayer/FooterPlayer.js
@@ -1,122 +1,130 @@
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import './Footer.css';
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import "./Footer.css";
 // import random from Math;
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, Redirect } from 'react-router-dom';
-import { useEffect, useRef, useState } from 'react';
-import { getAllSongs, getOneSong } from '../../store/song';
-import { addSongToPlaylist, nextSong } from '../../store/playlist';
-import { playingState, setPlayerTime, addSongToPlayer, setPlayer } from '../../store/player'
-import getCloudFrontDomain from '../../presignHelper';
-
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Redirect, Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { getAllSongs, getOneSong } from "../../store/song";
+import { addSongToPlaylist, nextSong } from "../../store/playlist";
+import {
+  playingState,
+  setPlayerTime,
+  addSongToPlayer,
+  setPlayer,
+} from "../../store/player";
+import getCloudFrontDomain from "../../presignHelper";
 
 function Footer() {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const song = useSelector(state => state.player.currentSong);
-    const songsObj = useSelector(state => state.songs.songs);
-    const playlist = useSelector(state => state.playlist.playlist);
-    const currentSongIdx = useSelector(state => state.playlist.currentSongIndex);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const song = useSelector((state) => state.player.currentSong);
+  const songsObj = useSelector((state) => state.songs.songs);
+  const playlist = useSelector((state) => state.playlist.playlist);
+  const currentSongIdx = useSelector(
+    (state) => state.playlist.currentSongIndex
+  );
 
-    let songs;
-    if (songsObj) {
-      songs = Object.values(songsObj);
+  let songs;
+  if (songsObj) {
+    songs = Object.values(songsObj);
+  }
+  const player = useRef(null);
+
+  const [signedSong, setSignedSong] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const signed = await getCloudFrontDomain(song?.url);
+      setSignedSong(signed);
+      await dispatch(getAllSongs());
+    })();
+  }, [song, dispatch]);
+
+  useEffect(() => {
+    dispatch(setPlayer(player));
+  }, [dispatch]);
+
+  // const changeSongPage = (e) => {
+  //   e.preventDefault();
+  //   dispatch(getOneSong(song.id));
+  //
+  //   history.push(`/songs/${song.id}`);
+  // };
+
+  const setPlay = async () => {
+    dispatch(playingState(true));
+  };
+
+  const setPause = () => {
+    dispatch(playingState(false));
+  };
+
+  const previousSong = () => {
+    if (currentSongIdx > 0) {
+      dispatch(nextSong("down"));
+      dispatch(addSongToPlayer(playlist[currentSongIdx - 1]));
+    } else {
+      player.current.audio.current.pause();
+      player.current.audio.current.currentTime = 0;
+      dispatch(setPlayerTime(0));
+      player.current.audio.current.play();
     }
-    const player = useRef(null);
+  };
 
-    const [signedSong, setSignedSong] = useState();
+  const setTime = (e) => {
+    if (e === 0) {
+      dispatch(setPlayerTime(0));
+    } else {
+      dispatch(setPlayerTime(e.srcElement.currentTime));
+    }
 
-    useEffect(() => {
-      (async () => {
-        const signed = await getCloudFrontDomain(song?.url);
-        setSignedSong(signed);
-        await dispatch(getAllSongs());
-      })()
-    }, [song, dispatch]);
-    
-    
-    useEffect(() => {
-      dispatch(setPlayer(player));
-    }, [dispatch]);
+    if (playlist?.length === 0) {
+      dispatch(addSongToPlaylist(song.id));
+    }
+    if (currentSongIdx === playlist?.length - 1) {
+      const songsLength = songs.length;
+      let randomSongIdx = Math.floor(Math.random() * songsLength);
+      while (songs[randomSongIdx].id === playlist[playlist.length - 1]) {
+        randomSongIdx = Math.floor(Math.random() * songsLength);
+      }
+      dispatch(addSongToPlaylist(songs[randomSongIdx].id));
+    }
+    if (currentSongIdx >= playlist?.length) {
+      dispatch(nextSong("down"));
+    }
+  };
 
-    const changeSongPage = (e) => {
-      e.preventDefault();
-      dispatch(getOneSong(song.id));
+  const newSong = () => {
+    if (playlist[currentSongIdx] === playlist[currentSongIdx + 1]) {
+      player.current.audio.current.pause();
+      player.current.audio.current.currentTime = 0;
+      dispatch(setPlayerTime(0));
+      player.current.audio.current.play();
+      dispatch(nextSong("up"));
+    } else {
+      setTime(0);
+      dispatch(nextSong("up"));
+    }
+  };
 
-      history.push(`/songs/${song.id}`);
-    };
+  const searchArtist = () => {
+    history.push(`/search?q=${song?.artist}`);
+  };
 
-    const setPlay = async () => {
+  useEffect(() => {
+    if (currentSongIdx) {
+      dispatch(addSongToPlayer(playlist[currentSongIdx]));
+      setPlay();
+      dispatch(setPlayerTime(0));
       dispatch(playingState(true));
-    };
-
-    const setPause = () => {
-      dispatch(playingState(false));
-    };
-
-    const previousSong = () => {
-      if (currentSongIdx > 0) {
-        dispatch(nextSong("down"));
-        dispatch(addSongToPlayer(playlist[currentSongIdx - 1]));
-      } else {
-        player.current.audio.current.pause();
-        player.current.audio.current.currentTime = 0;
-        dispatch(setPlayerTime(0));
-        player.current.audio.current.play();
-      }
-    };
-
-    const setTime = (e) => {
-      if (e === 0) {
-        dispatch(setPlayerTime(0));
-      } else {
-        dispatch(setPlayerTime(e.srcElement.currentTime));
-      }
-
-      if (playlist?.length === 0) {
-        dispatch(addSongToPlaylist(song.id));
-      }
-      if (currentSongIdx === playlist?.length - 1) {
-        const songsLength = songs.length;
-        let randomSongIdx = Math.floor(Math.random() * songsLength);
-        while (songs[randomSongIdx].id === playlist[playlist.length-1]) {
-          randomSongIdx = Math.floor(Math.random() * songsLength);
-        }
-        dispatch(addSongToPlaylist(songs[randomSongIdx].id));
-      }
-      if (currentSongIdx >= playlist?.length) {
-        dispatch(nextSong("down"));
-      }
-    };
-
-    const newSong = () => {
-      if(playlist[currentSongIdx] === playlist[currentSongIdx + 1]) {
-        player.current.audio.current.pause();
-        player.current.audio.current.currentTime = 0;
-        dispatch(setPlayerTime(0));
-        player.current.audio.current.play();
-        dispatch(nextSong("up"));
-      } else {
-        setTime(0);
-        dispatch(nextSong("up"));
-      }
-    };
-
-
-    useEffect(() => {
-      if (currentSongIdx) {
-        dispatch(addSongToPlayer(playlist[currentSongIdx]));
-        setPlay();
-        dispatch(setPlayerTime(0));
-        dispatch(playingState(true));
-      }
-    }, [currentSongIdx]);
-
+    }
+  }, [currentSongIdx]);
 
   return (
-    <footer className={song ? "footer" : "footer-hidden"} >
-      <AudioPlayer layout="horizontal-reverse"
+    <footer className={song ? "footer" : "footer-hidden"}>
+      <AudioPlayer
+        layout="horizontal-reverse"
         showSkipControls={true}
         showJumpControls={false}
         autoPlay={false}
@@ -129,15 +137,21 @@ function Footer() {
         onClickNext={newSong}
         onClickPrevious={previousSong}
         onEnded={newSong}
-        onCanPlay={e => setTime(0)}
+        onCanPlay={(e) => setTime(0)}
       />
-      <button
-        onClick={e => changeSongPage(e)}
-        className="player-button"
-      >
-        {song?.title}
-      </button>
-      <div className="player-title">{song?.artist}</div>
+      <div className="player-art-container">
+        <Link to={`/songs/${song?.id}`}>
+          <img className="player-art" src={song?.image_url} />
+        </Link>
+      </div>
+      <div className="player-text-info">
+        <Link to={`/songs/${song?.id}`}>
+          <div className="player-button">{song?.title}</div>
+        </Link>
+        <Link to={`/search?q=${song?.artist}`}>
+          <div className="player-title">{song?.artist}</div>
+        </Link>
+      </div>
     </footer>
   );
 }
